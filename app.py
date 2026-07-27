2e7b7efc52faa06b24539cb71a7d097dae56e100
jaramigo <18650551+jaramigo@users.noreply.github.com>
2026-07-27 15:25:01 -0400

    Crea interfaz principal

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
-from src.loader import cargar_documento
+import os
 
-RUTA_PDF = "data/PDF_politicas_devoluciones_TiendaBimBamBuy.pdf"
+import gradio as gr
+from dotenv import load_dotenv
+
+from config import INDEX_DIR
+from config import PDF_PATH
+from src.chain import answer_question
+from src.chain import build_chain
+from src.embeddings import build_embeddings
+from src.loaders import load_pdf
+from src.splitter import split_documents
+from src.vectorstore import build_or_load_vectorstore
+
+# Carga las variables de entorno
+load_dotenv()
+
+api_key = os.getenv("GOOGLE_API_KEY")
+if not api_key:
+    raise ValueError("GOOGLE_API_KEY no está configurada")
+
+os.environ["GOOGLE_API_KEY"] = api_key
 
 
-def main():
-    documentos = cargar_documento(RUTA_PDF)
-
-    print("=" * 60)
-    print("BimBamBuy AI Agent")
-    print("=" * 60)
-
-    print(f"Documento cargado correctamente")
-    print(f"Páginas cargadas: {len(documentos)}")
-
-    print("\nPrimeros 700 caracteres:\n")
-
-    print(documentos[0].page_content[:700])
-
-
-if __name__ == "__main__":
-    main()
+# Inicializa el sistema RAG
+def initialize_app():
