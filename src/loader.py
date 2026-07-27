c8d1850cbe7307ac57fa2b76f881a80f39c262db
jaramigo <18650551+jaramigo@users.noreply.github.com>
2026-07-27 15:11:34 -0400

    Preparación para leer los PDF

--- src/loader.py
+++ src/loader.py
@@ -1,8 +1,5 @@
 from langchain_community.document_loaders import PyPDFLoader
 
 
-def cargar_documento(ruta_pdf: str):
-    loader = PyPDFLoader(ruta_pdf)
-    documentos = loader.load()
-
-    return documentos
+def load_pdf(pdf_path):
+    return PyPDFLoader(str(pdf_path)).load()
