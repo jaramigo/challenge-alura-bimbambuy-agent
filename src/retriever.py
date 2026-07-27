f56a18e0a3ca382c846a6b55c7deec101418fac6
jaramigo <18650551+jaramigo@users.noreply.github.com>
2026-07-27 15:33:52 -0400

    Limpieza de duplicado

--- src/retriever.py
+++
@@ -1,8 +0,0 @@
-# Recupera el contexto más relevante
-def retrieve_context(retriever, question):
-    docs = retriever.get_relevant_documents(question)
-    context_parts = []
-    for doc in docs:
-        context_parts.append(doc.page_content)
-    context = "\n\n".join(context_parts)
-    return context
